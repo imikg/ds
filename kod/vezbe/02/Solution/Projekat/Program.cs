c630f599e72e00ec562451e1a88285db51a1a7f7
Dragor Draganović <dragor.draganovic@gmail.com>
2023-10-18 00:33:24 +0200

    Create (Kod): Vezbe 2
    Dodat kod sa drugih vezbi. Potrebno je dodati komentare i urediti.

---
+++ kod/vezbe/02/Solution/Projekat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Projekat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vec = new Vector(new double[] { 1, 2, 3, 4 });
+
+            foreach (var item in vec)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
